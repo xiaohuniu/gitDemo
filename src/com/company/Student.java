9da3477e55fcb19995aeadc4d15ee0e568c5e4af
xiaohuniu <15210225320@163.com>
2020-10-30 17:53:23 +0800

    在Main中添加bubblesort 新增Person和Student类

---
+++ src/com/company/Student.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Student {
+    private String name;
+    private String sex;
+    private String email;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
+
